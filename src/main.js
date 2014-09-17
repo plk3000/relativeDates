3d34c5bd1ffa6eb45916736a948eeb2544987265
Juan Diego Solis Smith <juandiego1987@gmail.com>
2014-09-17 23:40:02 +0000

    Initial files

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* relativeDates main */
+
+// Base function.
+var relativeDates = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+relativeDates.VERSION = '0.0.1';
+
+
+// Export to the root, which is probably `window`.
+root.relativeDates = relativeDates;
